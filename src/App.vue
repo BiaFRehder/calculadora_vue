e4920121dbeaf07bf2aa3fe2a1ef0971eb2a9de9
BiaFucuda <161872803+BiaFRehder@users.noreply.github.com>
2025-08-11 08:38:46 -0300

    Organizando o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container p-5 mt-4 text-center bg-light rounded-4">
+    <header>
+      <h1>Calculadora aritmética com Vue</h1>
+    </header>
+    <div class="row p-2 mt-5 bg-success rounded-4">
+      <div class="col">
+        <input class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número">
+      </div>
+      <div class="col">
+        <h5 class="h-100 fw-bolder d-flex align-items-center justify-content-center">+</h5>
+      </div>
+      <div class="col">
+        <input class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número:">
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row">
+      <div class="col">
+        <select class="mt-5 text-center rounded ">
+          <option value="adicionar">Adição (+)</option>
+          <option value="subtrair">Subtração (-)</option>
+          <option value="multiplicar">Multiplicação (*)</option>
+          <option value="dividir">Divisão (/)</option>
+          <option value="resto">Resto da divisão (%)</option>
+          <option value="elevado">Exponencial (^)</option>
+        </select>
+      </div>
+    </div>
+    <div class="result">
+      <h3 class="mt-5">Resultado</h3>
+      <h5 class="mt-5 fw-normal text-secondary">2 + 2 = <span class="fw-bolder text-success">4</span></h5>
+    </div>
+  </div>  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
