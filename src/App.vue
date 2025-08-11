85e43f5d2ca5e91421d80f55ebd9e352aa85a317
BiaFucuda <161872803+BiaFRehder@users.noreply.github.com>
2025-08-11 14:43:07 -0300

    criando as operações da calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,39 @@
 <script setup>
+import { reactive } from 'vue';
+
+  const estado = reactive({
+    number1: 0,
+    number2: 0,
+    operador: "+",
+  })
+
+
+  const calcular = () => {
+    const { operador, number1, number2 } = estado;
+
+    switch (operador) {
+      case "+":
+        return number1 + number2;
+      case "-":
+        return number1 - number2;
+      case "*":
+        return number1 * number2;
+      case "/":
+        if (number2 !== 0) {
+          return number1 / number2;
+        }else {
+          return "Não é possível dividir por zero"
+        }
+      case "%":
+        if (number2 !== 0) {
+          return number1 % number2;
+        }else {
+          return "Não é possível dividir por zero"
+        }
+      case "^":
+        return number1 ** number2;
+    }
+  }
 
 </script>
 
@@ -7,32 +42,34 @@
     <header>
       <h1>Calculadora aritmética com Vue</h1>
     </header>
-    <div class="row p-2 mt-5 bg-success rounded-4">
-      <div class="col">
-        <input class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número">
+    <form>
+      <div class="row p-2 mt-5 bg-success rounded-4">
+        <div class="col">
+          <input @keyup="evento => estado.number1 = Number(evento.target.value)" class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número">
+        </div>
+        <div class="col">
+          <h5 class="h-100 fw-bolder d-flex align-items-center justify-content-center">{{ estado.operador }}</h5>
+        </div>
+        <div class="col">
+          <input @keyup="evento => estado.number2 = Number(evento.target.value)" class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número:">
+        </div>
       </div>
-      <div class="col">
-        <h5 class="h-100 fw-bolder d-flex align-items-center justify-content-center">+</h5>
+      <div class="row">
+        <div class="col">
+          <select @change="evento => estado.operador = evento.target.value" class="mt-5 text-center rounded ">
+            <option value="+">Adição (+)</option>
+            <option value="-">Subtração (-)</option>
+            <option value="*">Multiplicação (*)</option>
+            <option value="/">Divisão (/)</option>
+            <option value="%">Resto da divisão (%)</option>
+            <option value="^">Exponencial (^)</option>
+          </select>
+        </div>
       </div>
-      <div class="col">
-        <input class="h-100 pt-4 pb-4 text-center" type="number" placeholder="Digite um número:">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <select class="mt-5 text-center rounded ">
-          <option value="adicionar">Adição (+)</option>
-          <option value="subtrair">Subtração (-)</option>
-          <option value="multiplicar">Multiplicação (*)</option>
-          <option value="dividir">Divisão (/)</option>
-          <option value="resto">Resto da divisão (%)</option>
-          <option value="elevado">Exponencial (^)</option>
-        </select>
-      </div>
-    </div>
+    </form>
     <div class="result">
       <h3 class="mt-5">Resultado</h3>
-      <h5 class="mt-5 fw-normal text-secondary">2 + 2 = <span class="fw-bolder text-success">4</span></h5>
+      <h5 class="mt-5 fw-normal text-secondary">{{ estado.number1 }} {{ estado.operador }} {{ estado.number2 }} = <span class="fw-bolder text-success">{{ calcular() }}</span></h5>
     </div>
   </div>  
 </template>
